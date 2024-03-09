36abd28abf0e2f5990ca25611238cb143b0e43ff
Samuel Narh <samuelnarh38@gmail.com>
2024-03-09 16:24:49 +0000

    added image path

--- routers/post.py
+++ routers/post.py
@@ -1,3 +1,6 @@
+import random
+import shutil
+import string
 from typing import List
 from fastapi import APIRouter,Depends, File, HTTPException, UploadFile,status
 from schemas.schemas import *
@@ -27,8 +30,18 @@ def get_all_post(db:Session=Depends(get_db)):
 
 
 # This allows us to upload images locally to access it
+#Static image file store
 @router.post('/image')
 def upload_image(image:UploadFile=File(...)):
-    pass
+    letters=string.ascii_letters
+    rand_str = ''.join(random.choice(letters) for i in range(5))
+    new = f'_{rand_str}.'
+    filename= new.join(image.filename.rsplit('.',1))
+    path = f'images/{filename}'
+    
+    with open(path,"w+b") as buffer:
+        shutil.copyfileobj(image.file,buffer)
+        
+    return {'filename': path}
     
     
