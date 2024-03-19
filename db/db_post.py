71ebdf3b9adcc6313122467f0159b69b875e3bc6
Samuel Narh <samuelnarh38@gmail.com>
2024-03-19 19:00:58 +0000

    updated th post delete

--- db/db_post.py
+++ db/db_post.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm.session import Session
 from schemas.schemas import *
 from db.models import DbPost
 from datetime import datetime
+from db.models import DbUser
 
 def create_post(request:PostBase,db:Session):
     new_post=DbPost(
@@ -22,18 +23,19 @@ def get_all_post(db:Session):
     return all_post
 
 def delete_post(db:Session,id:int,user_id:int):
-    post= db.query(DbPost).filter(DbPost.id==id).first()
-    if not post:
+    # Gets all dbpost
+    user_post= db.query(DbPost).filter(DbPost.id==id).first()
+    if not user_post:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail= f'user with id {id} not found'
         )
     # Actual user that creates the post to delete #if (database.id) != auth_id
-    if post.id != user_id:
+    if user_post.user_id != user_id:
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
             detail= "Only post creator can delete post"
         )
-    db.delete(post)
+    db.delete(user_post)
     db.commit()
     return "ok"
---
+++ routers/post.py
@@ -0,0 +1,50 @@
+import random
+import shutil
+import string
+from typing import List
+from fastapi import APIRouter,Depends, File, HTTPException, UploadFile,status
+from auth.oauth2 import get_current_user
+from schemas.schemas import *
+from sqlalchemy.orm.session import Session
+from db.database import get_db
+from db import db_post
+
+
+router= APIRouter(
+    prefix="/post",
+    tags=["post"]
+)
+image_url_types=['absolute','relative']
+
+#For auth current_user:UserAuth = Depends(get_current_user)
+@router.post('/',response_model=PostDisplay)
+def create_post(request:PostBase,db:Session=Depends(get_db),current_user:UserAuth = Depends(get_current_user)):
+    if not request.image_url_type in image_url_types:
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail="Parameter image_url_type can only take values 'absolute' or 'relative'"     
+        )
+    return db_post.create_post(request,db)
+
+@router.get('/all',response_model=List[PostDisplay])
+def get_all_post(db:Session=Depends(get_db)):
+    return db_post.get_all_post(db)
+
+
+# This allows us to upload images locally to access it
+#Static image file store
+@router.post('/image')
+def upload_image(image:UploadFile=File(...),current_user:UserAuth = Depends(get_current_user)):
+    letters=string.ascii_letters
+    rand_str = ''.join(random.choice(letters) for i in range(5))
+    new = f'_{rand_str}.'
+    filename= new.join(image.filename.rsplit('.',1))
+    path = f'images/{filename}'
+    
+    with open(path,"w+b") as buffer:
+        shutil.copyfileobj(image.file,buffer)
+    return {'filename': path}
+    
+@router.get('/delete/{id}') 
+def delete_post(id:int, db:Session=Depends(get_db),current_user:UserAuth = Depends(get_current_user)):
+    return db_post.delete_post(db,id,current_user.id) 
