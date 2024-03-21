5ea58798d92a93309aee2b196b3bf168ca6704de
Samuel Narh <samuelnarh38@gmail.com>
2024-03-21 16:47:23 +0000

    updated

--- db/database.py
+++ db/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
  
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./instagram.db'
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./funpost.db'
  
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
  
